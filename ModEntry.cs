c84444c877ff9008dcec4fd0d715f64033a930bb
rwanghm5 <rayden_wang@horacemann.org>
2026-05-28 15:48:36 -0400

    prelim steps for adding wapoints to teleportmenu

--- ModEntry.cs
+++ ModEntry.cs
@@ -6,11 +6,37 @@ namespace TeleportMod
 {
     public class ModEntry : Mod
     {
+            // Key used to store the waypoint in this save file's data.
+            private const string WaypointDataKey = "waypoint";
+
+            // The currently saved waypoint, or null if none. Read by TeleportMenu.
+            public (string Map, int X, int Y)? Waypoint { get; private set; }
         public override void Entry(IModHelper helper)
         {
             helper.Events.Input.ButtonPressed += OnButtonPressed;
+            helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+        // Load the saved waypoint (if any) when a save file is opened.
+        private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
+        {
+            var data = this.Helper.Data.ReadSaveData<WaypointData>(WaypointDataKey);
+            if (data != null)
+                Waypoint = (data.Map, data.X, data.Y);
+            else
+                Waypoint = null;
         }
 
+        // Called by TeleportMenu when the player saves their current position.
+        public void SaveWaypoint(string map, int x, int y)
+        {
+            Waypoint = (map, x, y);
+            this.Helper.Data.WriteSaveData(WaypointDataKey, new WaypointData
+            {
+                Map = map,
+                X   = x,
+                Y   = y
+            });
+        }
         private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
         {
             // Require a loaded save, player must be able to move 
@@ -26,5 +52,13 @@ namespace TeleportMod
                 Game1.activeClickableMenu = new TeleportMenu(this.Monitor);
             }
         }
+    }
+    // Serializable holder for the waypoint. SMAPI's data API needs a class with
+    // public properties (it can't serialize a C# tuple directly).
+    public class WaypointData
+    {
+        public string Map { get; set; } = "";
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
